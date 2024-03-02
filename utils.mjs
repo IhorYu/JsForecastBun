b186707848e15c7ac338398241c420fe5ebc6116
IhorYu <108236507+IhorYu@users.noreply.github.com>
2024-03-02 13:13:11 +0100

    Update utils.mjs
    Co-authored-by: Arthur <33233554+Mr-Arthur-White@users.noreply.github.com>

--- utils.mjs
+++ utils.mjs
@@ -37,7 +37,7 @@ export const addCity = async (city) => {
   await fs.writeFile("./cities.json", json);
 };
 
-export let cityObjValidate = object({
+export const cityObjValidate = object({
   longitude: number().required().min(-180).max(180),
   latitude: number().required().min(-90).max(90),
   name: string().required(),
