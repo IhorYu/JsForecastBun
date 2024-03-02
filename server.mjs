04c186a980fe6be48bd4ad3c52fec4cd97314082
IhorYu <ihoryukhnovskyi@gmail.com>
2024-03-02 01:52:03 +0100

    yup package integration and code refactor

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import {
   loadCities,
   getDataFromLogs,
   addCity,
-  // getParsedUrl,
+  cityObjValidate,
 } from "./utils.mjs";
 import { getForecastForCityIes } from "./index.mjs";
 
@@ -27,8 +27,10 @@ app.get("/cities", async (_, res) => {
     }, {});
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(citiesObj, null, 2));
+    return;
   } catch (error) {
     res.status(500).send("Internal Server Error");
+    return;
   }
 });
 
@@ -37,53 +39,60 @@ app.get("/forecast", async (_, res) => {
     const data = await getDataFromLogs();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data), null, 2);
+    return;
   } catch {
     res.status(500).send("Internal Server Error");
+    return;
   }
 });
 
-// TODO: .get('/cityForecast/:cityName', (...) => {...})     (query parameter > route parameter)
 app.get("/cityForecast/:cityName", async (req, res) => {
+  const cityName = req.params.cityName.toLowerCase();
+  if (!cityName) {
+    res.status(400).json({ error: "No city query parameter provided" });
+    return;
+  }
   try {
-    const cityName = req.params.cityName.toLowerCase();
-    if (cityName) {
-      const cityForecast = await getForecastForCityIes(cityName);
-      res.status(200).json(cityForecast);
-    } else {
-      res.status(400).json({ error: "No city query parameter provided" });
-    }
+    const cityForecast = await getForecastForCityIes(cityName);
+    res.status(200).json(cityForecast);
+    return;
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
+    return;
   }
 });
 
-app.get("/addCity", (req, res) => {
+app.get("/addCity", (_, res) => {
   res.sendFile(path.join(__dirname, "templates/add_city.html"));
+  return;
 });
-// TODO: move to post request (data would comes from body)
-app.post("/postAddCity", async (req, res) => {
+
+app.post("/city", async (req, res) => {
+  if (!req.body.city || !req.body.latitude || !req.body.longitude) {
+    res.status(400).send("Please provide city, latitude, and longitude");
+    return;
+  }
   try {
-    const city = req.body.city;
-    const latitude = req.body.latitude;
-    const longitude = req.body.longitude;
-    if (isNaN(+latitude) || isNaN(+longitude) || !isNaN(city)) {
-      res
-        .status(400)
-        .send("City must be string ,latitude and longitude must be numbers");
-    } else if (city && latitude && longitude) {
-      await addCity(city, latitude, longitude);
-      res.status(200).send("City added successfully");
-    } else {
-      res.status(400).send("Please provide city, latitude, and longitude");
-    }
+    const city = cityObjValidate.cast({
+      name: req.body.city.toLowerCase(),
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    });
+    await addCity(city);
+    res.status(200).send("City added successfully");
+    return;
   } catch (err) {
     console.error(err);
-    res.status(500).send("Internal Server Error");
+    res
+      .status(400)
+      .send(
+        `Invalid data input. Allowed data: city [string] - ${req.body.city}, latitude [number] - ${req.body.latitude}, longitude [number] - ${req.body.longitude}`
+      );
+    return;
   }
 });
 
-// TODO:  update to use variable
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`); // TODO: here as well
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
